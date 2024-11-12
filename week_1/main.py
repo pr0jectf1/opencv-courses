f35f3d0e349ebcadc6ee7751400c0a9e4ee588ec
pr0jectf1 <luis.diego.garcia.618@gmail.com>
2024-11-11 20:26:46 -0500

    QR Code detector feature

--- week_1/main.py
+++ week_1/main.py
@@ -1,23 +1,20 @@
 #QR CODE DETECTOR
+#Dects QR Code and creates a bounding box around it.
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
 matplotlib.rcParams['image.cmap'] = 'gray'
-img = cv2.imread("../images/qr.png")
+img = cv2.imread("./images/qr.png")
 
 qrDecoder = cv2.QRCodeDetector()
 opencvData, bbox, rectifiedImage = qrDecoder.detectAndDecode(img)
-print(rectifiedImage)
+n = len(bbox[0])
 
-if opencvData != None:
-    print("QR Code Detected")
-else:
-    print("QR Code NOT Detected")
-
-n = len(bbox)
-
-resultImagePath = "../images/QRCode-Output.png"
-cv2.imwrite(resultImagePath,img)
-plt.imshow(img)
-
+for x in range(n): 
+    print(x)
+    point2 = x +1
+    if x == n-1:
+        point2 = 0
+    cv2.line(img, (int(bbox[0][x][0]),int(bbox[0][x][1])), (int(bbox[0][point2][0]),int(bbox[0][point2][1])), (0,255,0))
+cv2.imwrite("./images/QRCode-Output.png", img)
